d26283ff8f62f9d15271d6ccc8c69894e7305fb9
harshbanipal <acidwave.3@gmail.com>
2025-12-27 15:13:03 -0500

    started node class

--- main.py
+++ main.py
@@ -8,6 +8,34 @@ pygame.display.set_caption("dijkstra")
 white = (255,255,255)
 
 # node class
+class Node():
+    # constructor
+    def __init__(self, x, y, radius, color, children):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.children = children
+
+    # getters
+    def getLoc(self):
+        return (self.x, self.y)
+    
+    def getColor(self):
+        return self.color
+    
+    def getRadius(self):
+        return self.radius
+    
+    def getChildren(self):
+        return self.children
+    
+    # setters
+    def setChildren(self, children):
+        self.children = children
+    
+
+    
 
 
 
