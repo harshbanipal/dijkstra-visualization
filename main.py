167ea9af8f33e43019116d4cd4782537f2afae51
harshbanipal <acidwave.3@gmail.com>
2025-12-27 15:55:32 -0500

    added dummy graph, need to implement drawing graph onto pygame window

--- main.py
+++ main.py
@@ -33,19 +33,24 @@ class Node():
     # setters
     def setChildren(self, children):
         self.children = children
-    
 
-    
+def main(): 
+    # dummy graph for now, will implement adding your own graph later
+    graph = {
+        'A': [('B', 1), ('D', 5), ('C', 3)],
+        'B': [('A', 1), ('D', 2)],
+        'C': [('A', 3), ('D', 1), ('E', 4)],
+        'D': [('A', 5), ('B', 2), ('C', 1), ('E', 7)],
+        'E': [('C', 4), ('D', 7)]
+    }
+        
+    # game loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
-
-# game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-
-pygame.quit()
-    
+    pygame.quit()
+        
