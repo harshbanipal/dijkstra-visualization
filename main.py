494e35cf7cfb9b1d7d7c3c2d0f1694dd21575162
harshbanipal <acidwave.3@gmail.com>
2025-12-28 22:33:32 -0500

    can draw my graph with node names and edge weights, started implementation of user drawing their own graph

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import pygame
+pygame.init()  
 
-pygame.init()   # initialize pygame
-
-screen = pygame.display.set_mode((640,640))    # create window with dimensions x by y
+# inititalize window
+screen = pygame.display.set_mode((1000,1000))    # create window 
 pygame.display.set_caption("dijkstra")
 
+
+# colors
 white = (255,255,255)
+black = (0,0,0)
+green = (0,255,0)
+red = (255,0,0)
+yellow = (255, 255, 0)
+
+
+# dummy graph for now, will implement adding your own graph later
+my_graph = {
+    'A': ([('B', 1), ('C', 5), ('D', 3)], (275, 350)),
+    'B': ([('A', 1), ('C', 2)], (625, 250)),
+    'C': ([('A', 5), ('B', 2), ('D', 1), ('E', 7)], (625, 450)),
+    'D': ([('A', 3), ('C', 1), ('E', 4)], (275, 550)),
+    'E': ([('D', 4), ('C', 7)], (625, 650))
+} 
+
+
+# text font
+text_font = pygame.font.SysFont('Arial', 12)
+
 
 # node class
 class Node():
@@ -34,23 +55,69 @@ class Node():
     def setChildren(self, children):
         self.children = children
 
-def main(): 
-    # dummy graph for now, will implement adding your own graph later
-    graph = {
-        'A': [('B', 1), ('D', 5), ('C', 3)],
-        'B': [('A', 1), ('D', 2)],
-        'C': [('A', 3), ('D', 1), ('E', 4)],
-        'D': [('A', 5), ('B', 2), ('C', 1), ('E', 7)],
-        'E': [('C', 4), ('D', 7)]
-    }
+def drawGraph(graph):
+    
+    for node in graph: 
+        node_pos = graph[node][1]
+
+        # draw edges
+        for child in graph[node][0]:
+            child_key = child[0]
+            child_pos = graph[child_key][1]
+            weight = child[1]
+            pygame.draw.line(screen, white, node_pos, child_pos)
+            
+            # find midpoint of line
+            if node_pos[0] < child_pos[0]:
+                weight_xpos = node_pos[0] + abs(node_pos[0] - child_pos[0])/2
+            else:
+                weight_xpos = child_pos[0] + abs(node_pos[0] - child_pos[0])/2
+
+            if node_pos[1] < child_pos[1]:
+                weight_ypos = node_pos[1] + abs(node_pos[1] - child_pos[1])/2
+            else:
+                weight_ypos = child_pos[1] + abs(node_pos[1] - child_pos[1])/2
+
+            drawText(str(weight), text_font, red, weight_xpos + 5, weight_ypos + 5)
+
+        # draw nodes
+        pygame.draw.circle(screen, white, node_pos, 25)
+        drawText(node, text_font, red, node_pos[0] - 5, node_pos[1] - 5)
+
         
+            
+    return
+
+
+def drawText(text, font, color, x, y,):
+    img = font.render(text, True, color)
+    screen.blit(img, (x,y))
+
+def main(): 
+     
+
     # game loop
-    running = True
+    running = True  
     while running:
+
+        screen.fill(black)
+
+        # draw my own graph
+        if pygame.mouse.get_pressed()[0] == True:
+            mouse_pos = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, white, mouse_pos, 25)
+            mouse_posStr = str(mouse_pos)
+            drawText(mouse_posStr, text_font, red, mouse_pos[0], mouse_pos[1])
+       
+        drawGraph(my_graph)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-
+        pygame.display.flip()
+    
     pygame.quit()
+
+main()
         
